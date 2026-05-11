94c9db9b8ae57c630d8aadad060f44977653eb8b
hyerin <luckyfor2020@naver.com>
2026-05-11 09:34:01 +0900

    feat: week6 ...

---
+++ week7/lionproject/lions/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Lion
+
+@admin.register(Lion)
+class LionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+        'track',
+        'created_at',
+    )
+
+    search_fields = (
+        'name',
+    )
+
+    list_filter = (
+        'track',
+    )
+
+    ordering = (
+        '-created_at',
+    )
+
+    list_per_page = 10
---
+++ week7/lionproject/lions/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+
+    path('lions/', views.lion_list, name='lion_list'),
+
+    path('lions/new/', views.lion_create, name='lion_create'),
+
+    path('lions/<int:id>/', views.lion_detail, name='lion_detail'),
+
+    path('lions/<int:id>/edit/', views.lion_edit, name='lion_edit'),
+
+    path('lions/<int:id>/delete/', views.lion_delete, name='lion_delete'),
+]
